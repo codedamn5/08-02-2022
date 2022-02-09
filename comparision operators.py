5c68927e908bd3a2d2f84804878de82bcf366232
codedamn5 <99306866+codedamn5@users.noreply.github.com>
2022-02-09 10:14:22 +0530

    Add files via upload

---
+++ comparision operators.py
@@ -0,0 +1,11 @@
+print('comparision operators')
+a=int(input('enter a number:'))
+b=int(input('enter a number:'))
+print(a<b)
+print(a>b)
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
